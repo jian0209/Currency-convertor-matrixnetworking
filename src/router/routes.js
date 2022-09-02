ceb809a306d61e1a67daf5e488e23f96b9aa9a6c
jian0209 <95278074+jian0209@users.noreply.github.com>
2022-09-02 11:25:43 +0800

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,16 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
   },
+  {
+    path: '/ndosifjwqe2sdcoi213',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/bitopSplashScreen/IndexPage.vue')
+      }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
