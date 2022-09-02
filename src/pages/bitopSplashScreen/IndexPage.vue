167e06ecbbf47fc7ade6f7a2738607fa3034da5c
tey kai jian <jian0209@teys-MacBook-Air.local>
2022-09-02 11:27:31 +0800

    add bitop splash screen

---
+++ src/pages/bitopSplashScreen/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page style="display: flex; justify-content: center; align-items: center"
+    ><q-img src="./img/logo.jpeg" height="40vh" width="40vh"
+  /></q-page>
+</template>
+  
+  <script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    document.title = 'Term&Condition-Currency'
+
+    const tab = ref('userAgreement')
+    const splitterModel = ref(20)
+
+    return { tab, splitterModel }
+  }
+})
+</script>
+  
+  <style>
+.inside-cont-term-privacy {
+  padding: 10vh 10vw;
+}
+
+.tabColor {
+  color: #e8aa10;
+}
+
+.block {
+  font-size: 21px;
+}
+
+@media screen and (max-width: 910px) {
+  .block {
+    font-size: 17px;
+  }
+}
+</style>
+  
